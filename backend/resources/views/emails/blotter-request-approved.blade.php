d18e5aa3ec090001da8d86af381de935000b8b6b
gecamichaelaeron <gecaichaelaeron@gmail.com>
2025-11-25 03:01:43 +0800

    b1

--- backend/resources/views/emails/blotter-request-approved.blade.php
+++ backend/resources/views/emails/blotter-request-approved.blade.php
@@ -593,8 +593,8 @@
             <!-- Success Banner -->
             <div class="success-banner">
                 <div class="success-icon">
-                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z"/>
+                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width="24" height="24">
+                        <path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z" fill="white"/>
                     </svg>
                 </div>
                 <div class="success-banner-content">
@@ -606,8 +606,8 @@
             <div class="info-card">
                 <div class="info-card-header">
                     <div class="info-card-icon">
-                        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z"/>
+                        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width="20" height="20">
+                            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z" fill="white"/>
                         </svg>
                     </div>
                     <h3>Request Details</h3>
@@ -636,8 +636,8 @@
             <div class="warning-box">
                 <div class="warning-header">
                     <div class="warning-icon">
-                        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M1 21h22L12 2 1 21zm12-3h-2v-2h2v2zm0-4h-2v-4h2v4z"/>
+                        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width="20" height="20">
+                            <path d="M1 21h22L12 2 1 21zm12-3h-2v-2h2v2zm0-4h-2v-4h2v4z" fill="white"/>
                         </svg>
                     </div>
                     <strong>Important Reminder</strong>
